002afd0f7b1993f94d7018bbd482b3d813f337c3
Michael Kaiser <michael.kaiser@conplement.de>
2024-10-28 11:47:50 +0100

    x

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+certbot -n -d cpvoting-env.eba-qv8ejmp5.eu-central-1.elasticbeanstalk.com certonly --nginx --agree-tos --email michael.kaiser@conplement.de
---
+++ .platform/hooks/postdeploy/01_move_nginx_enable_https_config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "test"
+mv /tmp/nginx/https.txt /etc/nginx/conf.d/00_enable_https.conf
